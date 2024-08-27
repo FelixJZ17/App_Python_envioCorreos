dff630e8fb99c52dbee56c5fa2e8d6b68625fea3
FelixJZ17 <felix_jimenez9@hotmail.com>
2024-08-27 21:19:32 +0200

    implementamos una firma basica del mail

--- README.txt
+++ README.txt
@@ -15,3 +15,20 @@ EMAIL_PASSWORD="" # ejemplo: contraseña_ejemplo
 EMAIL_EMISOR=""  #ejemplo: hola_ejemplo@gmail.com
 SERVER_SMTP=""   # ejemplo: "smtp.gmail.com"
 PORT=465          ### --> para SSL se usa el 465, para el TLS se usa el 587 
+
+
+-----------------
+
+Para crear la firma de los mails, he usado el tutorial:
+https://www.youtube.com/watch?v=W7JIdLU23GI&pp=ygUnc2lnbiBweXRob24gbWFpbCB3aXRoIHZhbGlkIGNlcnRpZmljYXRl
+
+Luego tilizando OPENSSL - primero instalando en Windows OPENSSL, haciendo una instalación normal. 
+Y luego agregando a las variables de entorno el bin de la raiz de OPENSSL, para que funionen los comandos.  
+
+y luego instalando en Python la librería: 
+> pip install pyOpenSSL
+
+todo estos lo hago dentro de una carpeta "certs" que no estará visible en el código, ya que contiene las claves de firma y no quiero compartirlas. 
+
+tambien para utilizar los mails firmados, tenemos que instalar:
+> pip install python-smail
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import ssl
 import smtplib
 from dotenv import load_dotenv
 
+
 ### importamos las librerias para trabajar con el archivo adjunto
 import email.mime.multipart
 import email.mime.base
 from email.mime.text import MIMEText
+
+## para firmar el mensaje tengo que importar
+from smail import sign_message
 
 # Cargo las variables de entorno
 load_dotenv()
@@ -16,15 +20,18 @@ email_emisor= os.getenv("EMAIL_EMISOR")
 email_contrasena=os.getenv("EMAIL_PASSWORD")
 servicio_smtp=os.getenv("SERVER_SMTP")
 port_smtp=os.getenv("PORT")
+cert_file = os.getenv("CERT_FILE_PATH")
+key_file = os.getenv("KEY_FILE_PATH")
 
 #aquí el mail al que le voy a enviar
 email_receptor = "fjimenezgestiones@gmail.com"
 
 #aquí los detalles del mail al que le voy a enviar. 
-asunto="Revisa tu correo con adjunto"
+asunto="Revisa tu correo con adjunto y con firma"
 cuerpo="""
 Este será el cuerpo del correo, 
 Que se puede poner en varios saltos de línea y darle el formato que queramos. 
+Además quiero informarte que el correo está firmado. 
 """
 
 ## aquí hago simple comprobación de que está cogiendo bien las variables de entorno. 
@@ -50,14 +57,29 @@ email.encoders.encode_base64(adjunto)
 adjunto.add_header('Content-Disposition', 'attachment; filename= %s' %nombre_archivo)
 em.attach(adjunto)
 
+## aquí procedemos a firmar el mail con la librería smail
+sign_mail = sign_message(em, key_file, cert_file)
+
 # tenemos que crear un contexto para luego pasarselo al smtplib.SMTP_SSL
 contexto = ssl.create_default_context()
 
 with smtplib.SMTP_SSL(servicio_smtp, port_smtp, context=contexto) as smtp:
     # Abrir la conexion y loguearnos con nuestro usuario y contraseña. 
     smtp.login(email_emisor, email_contrasena)
-    # Enviar el mail
-    smtp.sendmail(email_emisor, email_receptor, em.as_string())
+    
+    # Enviar el mail sin firmar, dejo comentada esta línea
+    ### smtp.sendmail(email_emisor, email_receptor, em.as_string())
+
+    #Enviar mail firmado
+    smtp.send_message(sign_mail)
+    ### lamentablemente, despues de seguir todo, como la firma no está validada por ningún organismo válido
+    ### Aun así, ya evito que el mail vaya a SPAM directamente o que incluso en el asunto me pongo [SPAM] como antes..., 
+    ### según el tutorial seguido, se podría sustituir por una firma válida, por ejemplo de la fnmt (o tendré que buscar más info) 
+    ### pero entonces me faltaría saber que tengo que poner en key_file y que poner en cert_file, ya que para la firma
+    ### yo lo selecciono directamente desde windows siempre que quiero firmar algo....
+    ### de momento lo dejo así. 
+
+
     # cerrar la conexion smtp
     smtp.quit()
 
